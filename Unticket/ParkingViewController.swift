bd237de520fe7ecf6bd30d37af4eb9a8068d846a
aneesa <aneesayoosef246@gmail.com>
2021-05-22 22:10:48 +0530

    Outlets added and Picker view added

--- Unticket/ParkingViewController.swift
+++ Unticket/ParkingViewController.swift
@@ -1,20 +1,45 @@
 
 import UIKit
 import MapKit
-class ParkingViewController: UIViewController {
-
+class ParkingViewController: UIViewController,UIPickerViewDataSource, UIPickerViewDelegate{
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+            return arrayOf24.count
+        }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String?
+       {
+               return String(arrayOf24[row])
+           
+       }
+       
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+           
+           //let hourValue = arrayOf24[time.selectedRow(inComponent: 0)]
+           //let minutelValue = arrayOf60[time.selectedRow(inComponent: 1)]
+           
+               print(String(arrayOf24[row]))
+       }
+    var arrayOf24 = Array(0...24)
+    var arrayOf60 = Array(0...60)
     @IBOutlet weak var time: UIPickerView!
-    @IBOutlet weak var buildingCode: UIPickerView!
+    
     @IBOutlet weak var timestamp: UITextField!
     @IBOutlet weak var suitNo: UITextField!
     @IBOutlet weak var address: UITextField!
     
+    @IBOutlet weak var LicenseplateNo: UITextField!
+    
+    @IBOutlet weak var buildingCode: UITextField!
+    
     @IBOutlet weak var mapLocation: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
-//        self.time.dataSource = self
-//        self.time.delegate = self
-        // Do any additional setup after loading the view.
+        time.delegate = self
+        time.delegate?.pickerView?(time, didSelectRow: 0, inComponent: 0)
+              
     }
     
     @IBAction func addParking(_ sender: Any) {
@@ -22,19 +47,4 @@ class ParkingViewController: UIViewController {
     
 }
 
-extension ViewController: UIPickerViewDataSource{
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return 5;
-    }
-    
-    
-}
-extension ViewController:  UIPickerViewDelegate{
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return "test"
-    }
-}
+
